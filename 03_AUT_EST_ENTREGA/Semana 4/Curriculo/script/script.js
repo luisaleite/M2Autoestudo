a445f661ec55aa3f20ea7c2541f65b4d85ad3b49
luisaleite <99203030+luisaleite@users.noreply.github.com>
2022-05-17 10:06:43 -0300

    .

---
+++ 03_AUT_EST_ENTREGA/Semana 4/Curriculo/script/script.js
@@ -0,0 +1,11 @@
+let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+        if(meuSrc === 'imagem/luisa.jpg') {
+        minhaImagem.setAttribute ('src','imagem/luisa2.jpg');
+        } 
+        else {
+        minhaImagem.setAttribute ('src','imagem/luisa.jpg');
+        }
+}
